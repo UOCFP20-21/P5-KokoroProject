56b050626123b2857f939c45e714e2974714a24a
farellanoc <farellano@uoc.edu>
2020-10-04 21:04:10 +0200

    Commit

--- src/main/java/es/kokoro/model/Ong.java
+++ src/main/java/es/kokoro/model/Ong.java
@@ -24,4 +24,65 @@ public class Ong {
      * Listado Proyectos
      */
     private List<Proyecto> ProyectosList;
+    /**
+     * Nombre Sede
+     */
+    private Sede Sede;
+
+    public String getNombreOng() {
+        return NombreOng;
+    }
+
+    public void setNombreOng(String nombreOng) {
+        NombreOng = nombreOng;
+    }
+
+    public List<Delegacion> getDelegacionList() {
+        return DelegacionList;
+    }
+
+    public void setDelegacionList(List<Delegacion> delegacionList) {
+        DelegacionList = delegacionList;
+    }
+
+    public List<Ingresos> getIngresosList() {
+        return IngresosList;
+    }
+
+    public void setIngresosList(List<Ingresos> ingresosList) {
+        IngresosList = ingresosList;
+    }
+
+    public List<Socio> getSociosList() {
+        return SociosList;
+    }
+
+    public void setSociosList(List<Socio> sociosList) {
+        SociosList = sociosList;
+    }
+
+    public List<Proyecto> getProyectosList() {
+        return ProyectosList;
+    }
+
+    public void setProyectosList(List<Proyecto> proyectosList) {
+        ProyectosList = proyectosList;
+    }
+
+    public es.kokoro.model.Sede getSede() {
+        return Sede;
+    }
+
+    public void setSede(es.kokoro.model.Sede sede) {
+        Sede = sede;
+    }
+
+    public Ong(String nombreOng, List<Delegacion> delegacionList, List<Ingresos> ingresosList, List<Socio> sociosList, List<Proyecto> proyectosList, es.kokoro.model.Sede sede) {
+        NombreOng = nombreOng;
+        DelegacionList = delegacionList;
+        IngresosList = ingresosList;
+        SociosList = sociosList;
+        ProyectosList = proyectosList;
+        Sede = sede;
+    }
 }
