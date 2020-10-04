745f64cebd53e6f53a5828c5560cb1f8c0c1f746
farellanoc <farellano@uoc.edu>
2020-10-04 19:56:11 +0200

    Clase Ong

--- src/main/java/es/kokoro/model/Ong.java
+++ src/main/java/es/kokoro/model/Ong.java
@@ -1,4 +1,27 @@
 package es.kokoro.model;
 
+import java.util.List;
+
 public class Ong {
+
+    /**
+     * Nombre Ong
+     */
+    private String NombreOng;
+    /**
+     *Listado Delegaciones
+     */
+    private List<Delegacion> DelegacionList;
+    /**
+     *Listado Ingresos
+     */
+    private List<Ingresos> IngresosList;
+    /**
+     *Listado Socios
+     */
+    private List<Socio> SociosList;
+    /**
+     *Listado Proyectos
+     */
+    private List<Proyecto> ProyectosList;
 }
