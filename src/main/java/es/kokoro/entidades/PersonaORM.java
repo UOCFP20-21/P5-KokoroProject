4095c1506f22d5b4618fa93fe827faba12f629a3
fran <farellanoc@uoc.edu>
2020-11-25 20:08:04 +0100

    PersonaORM

--- src/main/java/es/kokoro/entidades/PersonaORM.java
+++ src/main/java/es/kokoro/entidades/PersonaORM.java
@@ -5,12 +5,11 @@
  */
 package es.kokoro.entidades;
 
-import es.kokoro.model.Socio;
+import es.kokoro.model.Persona;
 
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,7 +17,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -38,7 +36,7 @@ import javax.xml.bind.annotation.XmlRootElement;
         , @NamedQuery(name = "Personas.findEmail", query = "SELECT * FROM email WHERE email = :email")
         , @NamedQuery(name = "Personas.findByFechaNac", query = "SELECT * FROM fechaNac WHERE fechaNac = :fechaNac")})
 
-public class Persona implements Serializable {
+public class PersonaORM extends Persona implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -64,20 +62,18 @@ public class Persona implements Serializable {
     private String email;
     @Column(name = "fechaNac")
     private Date fechaNac;
-    //@OneToOne(cascade = CascadeType.ALL, mappedBy = "persona")
-    //private Socios socios;
 
-    public Persona() {
+
+    public PersonaORM(Long idPersona, String nombre, String apellidos, String identificador, String nacionalidad, String direccion, String poblacion, String telefono, String email, Date fechaNac) {
+        super(idPersona, nombre, apellidos, identificador, nacionalidad, direccion, poblacion, telefono, email, fechaNac);
     }
 
-    public Persona(Integer idpersona) {
-        this.idpersona = idpersona;
-    }
+   /* public PersonaORM(Integer idpersona) { this.idpersona = idpersona;
+    } */
 
     public Integer getIdpersona() {
         return idpersona;
     }
-
     public void setIdpersona(Integer idpersona) {
         this.idpersona = idpersona;
     }
@@ -85,7 +81,6 @@ public class Persona implements Serializable {
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -93,66 +88,52 @@ public class Persona implements Serializable {
     public String getApellidos() {
         return apellidos;
     }
-
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
-    public String getIndentidicador() {
-        return identificador;
-    }
-
+    public String getIdentidicador() { return identificador; }
     public void setIdentificador(String identificador) {
         this.identificador = identificador;
     }
+
     public String getNacionalidad() {
         return nacionalidad;
     }
-
     public void setNacionalidad(String nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
+
     public String getDireccion() {
         return direccion;
     }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
 
-    public void setDireccion(String Direccion) {
-        this.direccion = direccion;
-    }
     public String getPoblacion() {
         return poblacion;
     }
-
-    public void setPoblacion(String Poblacion) {
+    public void setPoblacion(String poblacion) {
         this.poblacion = poblacion;
     }
+
     public String getTelefono() {
         return telefono;
     }
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public void setTelefono(String telefono) { this.telefono = telefono; }
 
-    }public String getEmail() {
+    public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
     }
+
     public Date getFechaNac() {
         return fechaNac;
     }
-
     public void setFechaNac(Date fechaNac) {
         this.fechaNac = fechaNac;
     }
-
-   /*** public Socio getSocio() {
-        return socio;
-    }
-
-    public void setSocio(Socio socio) {
-        this.socio = socio;
-    } ***/
 
 
     @Override
@@ -165,23 +146,19 @@ public class Persona implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Persona)) {
+        if (!(object instanceof PersonaORM)) {
             return false;
         }
-        Persona other = (Persona) object;
+        PersonaORM other = (PersonaORM) object;
         if ((this.idpersona == null && other.idpersona != null) || (this.idpersona != null && !this.idpersona.equals(other.idpersona))) {
             return false;
         }
         return true;
     }
 
-    /*** @Override
+    @Override
     public String toString() {
         return "Entidades.Persona [idpersona=" + idpersona + ", nombre=" + nombre + ", apellidos=" + apellidos + ", identificador=" + identificador + "]";
     }
-    @Override
-    public String toString() {
-        return "Entidades.Persona[ idpersona=" + idpersona + " ]";
-    } ***/
 
 }
